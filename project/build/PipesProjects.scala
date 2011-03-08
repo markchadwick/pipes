20c82c3106d6b00cbed7b173e95fad60d24cc2b0
Mark Chadwick <mark.chadwick@gmail.com>
2011-03-08 11:54:16 -0800

    Moved pipes to pipes-core

--- project/build/PipesProjects.scala
+++ project/build/PipesProjects.scala
@@ -1,7 +1,26 @@
 import sbt._
 import reaktor.scct.ScctProject
 
-class PipesProject(info: ProjectInfo) extends DefaultProject(info)
-                                      with ScctProject {
-  lazy val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
+class PipesProject(info: ProjectInfo) extends ParentProject(info) {
+
+  val core = project("pipes-core", "pipes-core", new PipesCore(_))
+
+  class PipesCore(info: ProjectInfo) extends DefaultProject(info)
+                                     with ScctProject {
+    DependsOn.scalatest
+  }
+
+  // //////////////////////////////////////////////////////////////////////////
+  // Examples
+
+  val jgrep = project("examples" / "jgrep", "jgrep", new JGrep(_), core)
+
+  class JGrep(info: ProjectInfo) extends DefaultProject(info) {
+    DependsOn.scalatest
+  }
+
+  object DependsOn {
+    lazy val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
+  }
 }
+
